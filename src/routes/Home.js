dd4cdc61f8023668d40122bba032a847d8ca575d
lani <laniiiiworld@gmail.com>
2023-10-18 17:37:51 +0900

    Implement Headline Component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,13 @@
+import Headline from '../components/Headline';
 import { Component } from '../core/common';
 
 export default class Home extends Component {
   render() {
-    this.el.innerHTML = /* html */ `
-      <h1>Home Page!</h1>
-    `;
+    const $headline = new Headline().el;
+
+    this.el.classList.add('container');
+    this.el.append(
+      $headline, //
+    );
   }
 }
--- src/store/message.js
+++
@@ -1,5 +0,0 @@
-import { Store } from '../core/common';
-
-export default new Store({
-  message: 'Hello~',
-});
