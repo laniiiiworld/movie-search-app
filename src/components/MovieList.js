540aa491504714897e6fc3d7a958d23d04e0f9f9
lani <laniiiiworld@gmail.com>
2023-10-20 23:00:07 +0900

    Implement loading animation

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,17 +6,23 @@ export default class MovieList extends Component {
   constructor() {
     super();
     movieStore.subscribe('movies', () => this.render());
+    movieStore.subscribe('loading', () => this.render());
   }
 
   render() {
     const { movies } = movieStore.state;
-    if (!movies.length) return;
 
     this.el.classList.add('movie-list');
     this.el.innerHTML = /* html */ `
       <div class="movies"></div>
+      <div class="the-loader hide"></div>
     `;
     const $movies = this.el.querySelector('.movies');
     $movies.append(...movies.map((movie) => new MovieItem({ props: { movie } }).el));
+
+    const $loader = this.el.querySelector('.the-loader');
+    movieStore.state.loading
+      ? $loader.classList.remove('hide') //
+      : $loader.classList.add('hide');
   }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,11 +5,13 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
+  loading: false,
 });
 
 export default store;
 
 export const searchMovies = async (page) => {
+  store.state.loading = true;
   store.state.page = page;
   if (page === 1) {
     store.state.movies = [];
@@ -26,4 +28,5 @@ export const searchMovies = async (page) => {
     ...store.state.movies, //
     ...Search,
   ];
+  store.state.loading = false;
 };
